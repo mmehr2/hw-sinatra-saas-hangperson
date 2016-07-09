87416a7c801253e7453d789a9481a99437d713d8
mike <macdev@azuresults.com>
2016-07-09 21:04:21 +0000

    Passing cucumber features/game_over.feature

--- app.rb
+++ app.rb
@@ -56,7 +56,12 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
+    case @game.check_win_or_lose
+      when :win
+        redirect '/win'
+      when :lose
+        redirect '/lose'
+    end
     erb :show # You may change/remove this line
   end
   
